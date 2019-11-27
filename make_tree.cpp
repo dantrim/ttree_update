ff61a53bacce0683e08957c0d172edf5cd604bf1
Daniel Joseph Antrim <daniel.joseph.antrim@cern.ch>
2019-11-27 14:52:20 -0800

    initial commit

---
+++ make_tree.cpp
@@ -0,0 +1,37 @@
+//std/stl
+#include <iostream>
+#include <string>
+using namespace std;
+
+//ROOT
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+
+int main()
+{
+    string ofilename = "tree.root";
+    TFile* rfile = TFile::Open(ofilename.c_str(), "RECREATE");
+
+    TTree* tree = new TTree("mytree", "mytree");
+    float var0 = 0.0;
+    float var1 = 0.0;
+    float var2 = 0.0;
+    tree->Branch("var0", &var0);
+    tree->Branch("var1", &var1);
+    tree->Branch("var2", &var2);
+
+    unsigned int n_events = 1000;
+    for(size_t ievent = 0; ievent <  n_events; ievent++)
+    {
+        var0 = 2;
+        var1 = 2;
+        var2 = 49;
+        tree->Fill();
+    } // ievent
+
+    tree->Write();
+    rfile->Close();
+
+    return 0;
+}
---
+++ update_tree.cpp
@@ -0,0 +1,41 @@
+//std/stl
+#include <iostream>
+#include <string>
+using namespace std;
+
+//ROOT
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+
+int main()
+{
+    string ifilename = "tree.root";
+    TFile* rfile = TFile::Open(ifilename.c_str(), "UPDATE");
+    TTree* tree = static_cast<TTree*>(rfile->Get("mytree"));
+    float var0 = 0.0;
+    float var1 = 0.0;
+    float varN = 0.0;
+
+    TBranch* new_branch = tree->Branch("varN", &varN, "varN/F");
+    tree->SetBranchStatus("*",0);
+    tree->SetBranchStatus("var0", 1);
+    tree->SetBranchStatus("var1", 1);
+    tree->SetBranchStatus("varN", 1);
+    tree->SetBranchAddress("var0", &var0);
+    tree->SetBranchAddress("var1", &var1);
+
+    unsigned int n_events = tree->GetEntries();
+    for(size_t ievent = 0; ievent < n_events; ievent++)
+    {
+        tree->GetEvent(ievent);
+        varN = var0 + var1;
+        new_branch->Fill();
+    } // ievent
+
+    tree->SetBranchStatus("*",1);
+    tree->AutoSave();
+    rfile->Close();
+
+    return 0;
+}
